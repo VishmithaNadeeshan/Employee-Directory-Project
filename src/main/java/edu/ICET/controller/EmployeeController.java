aa2797b35839ff90196c1590e1c389fd91c0ae6b
VishmithaNadeeshan <vishmithanadeeshan20041111@gmail.com>
2025-04-29 11:01:18 +0530

    Initialized EmployeeController

--- src/main/java/edu/ICET/controller/EmployeeController.java
+++ src/main/java/edu/ICET/controller/EmployeeController.java
@@ -1,4 +1,33 @@
 package edu.ICET.controller;
 
+import edu.ICET.dto.Employee;
+import edu.ICET.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin
+@RequestMapping("/employee")
 public class EmployeeController {
+    final EmployeeService service;
+    @PostMapping("/add")
+    public void addEmployee(@RequestBody Employee employee){
+        service.addEmployee(employee);
+    }
+    @PutMapping("/update")
+    public void updateEmployee(@RequestBody Employee employee){
+        service.updateEmployee(employee);
+    }
+    @DeleteMapping("/deleteById/{id}")
+    public void deleteEmployee(@PathVariable Integer id){
+        service.deleteEmployee(id);
+    }
+    @GetMapping("get-All")
+    public List<Employee>getAll(){
+       return service.getAll();
+    }
+
 }
